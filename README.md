76909064d79ba22efb34cfe3886027eb215c3b3b
Nicolas Leclerc <nl@spirotron.fr>
2017-08-26 23:17:04 +0200

    Initial commit. First release.

---
+++ README.md
@@ -0,0 +1,7 @@
+# NightClock
+
+## Description
+A Cordova app that displays current time and date in a way suitable to be displayed in the dark in your bedroom.
+
+## License
+[GPL v2 or later](LICENSE)
---
+++ www/js/index.js
@@ -0,0 +1,52 @@
+var app = {
+	initialize: function() {
+		document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
+		app.appEl = $('.app');
+
+		app.hoursEl = $('.time .hours');
+		app.minutesEl = $('.time .minutes');
+		app.dateEl = $('.date');
+
+		moment.locale(app.getNavigatorLanguage());
+
+		app.startUpdateTimer();
+		app.appEl.addClass('initialized')
+	},
+
+	getNavigatorLanguage: function() {
+		var result = navigator.language || navigator.userLanguage;
+		return result.replace(/-??$/,'');
+	},
+
+	startUpdateTimer: function() {
+		app.updateTime();
+		app.timer = setInterval(app.updateTime,1000);
+	},
+
+	stopUpdateTimer: function() {
+		if (app.timer) {
+			stopInterval(app.timer);
+			delete app.timer;
+		}
+	},
+
+	updateTime: function() {
+		var currentDate = moment();
+
+		app.hoursEl.text(currentDate.format('HH'));
+		app.minutesEl.text(currentDate.format('mm'));
+		app.dateEl.text(currentDate.format('LLLL').replace(/ .*$/,' ')+currentDate.format('LL').replace(/ [^ ]*$/,''));
+	},
+
+	onDeviceReady: function() {
+		window.plugins.insomnia.keepAwake();
+		AndroidFullScreen.leanMode(function(){
+			console.log('Lean mode activated.');
+		}, function(err) {
+			console.error('Error setting lean mode:',err);
+		});
+	}
+};
+
+app.initialize();
